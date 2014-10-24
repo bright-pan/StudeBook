db9323c9c5824fc4622f5cecfc9678a2ba1db578
Justin X. Lambeck <lambeck.j@gmail.com>
2014-10-24 15:21:30 +0200

    User Pages

--- APP/routers/urls.py
+++ APP/routers/urls.py
@@ -1,10 +1,18 @@
 #DJANGO
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from django.views.generic import ListView
+
 #APP
 from APP.views.APPView import APPView
 from APP.views.LoginView import LoginView
+from APP.views.ProfileView import ProfileView
+from APP.views.PagesView import PagesView
 
 urlpatterns = patterns('',
 	url(r'^$', APPView.as_view(), name = 'APP'),
 	url(r'^login$', LoginView.as_view(), name = 'Login'),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^profile/', ProfileView.as_view()),
+	url(r'^pages/', PagesView.as_view()),
 )
