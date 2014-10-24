cdd1a6396185c11cd4b5306bc048a789068b4494
Justin X. Lambeck <lambeck.j@gmail.com>
2014-10-24 15:44:30 +0200

    Page details

--- APP/routers/urls.py
+++ APP/routers/urls.py
@@ -8,11 +8,13 @@ from APP.views.APPView import APPView
 from APP.views.LoginView import LoginView
 from APP.views.ProfileView import ProfileView
 from APP.views.PagesView import PagesView
+from APP.views.PageView import PageView
 
 urlpatterns = patterns('',
 	url(r'^$', APPView.as_view(), name = 'APP'),
 	url(r'^login$', LoginView.as_view(), name = 'Login'),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^profile/', ProfileView.as_view()),
-	url(r'^pages/', PagesView.as_view()),
+	url(r'^pages/$', PagesView.as_view()),
+	url(r'^pages/(?P<pageID>\w+)/$', PageView.as_view()),
 )
