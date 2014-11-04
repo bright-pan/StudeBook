3e654c62e33f798b81e96f837a1a3b8f142ab0a0
mjansma <m.jansma@live.nl>
2014-11-04 21:40:27 +0100

    CHAT commit

--- APP/views/MainView.py
+++ APP/views/MainView.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 #SB
 from APP.models.UserLoginModel import UserLogin
+from APP.models.UserFriendModel import UserFriend
 
 
 """
@@ -13,32 +14,37 @@ from APP.models.UserLoginModel import UserLogin
 """
 
 class MainView(View) :
-    
-    #Get session data 
+
+    #Get session data
     def getSessionValue (self, request, key, default = False) :
         return request.session.get(key, default);
 
     #Get user login state
     def isLoggedIn (self, request) :
         return self.getSessionValue(request, 'logged_in');
-    
+
+    #Get user friend by user
+    def getUserFriend (self, request) :
+        if (self.isLoggedIn(request)) :
+            return UserFriend.getUserFriendByUser(self.getUserLogin(request).user);
+
     #Get user login instance
     def getUserLogin (self, request) :
         if (self.isLoggedIn(request)) :
             return UserLogin.objects.get(user_login_id = self.getSessionValue(request, 'user_login_id'));
         return False;
-    
-    #Render template 
+
+    #Render template
     def render (self, request, template, params) :
         #TMP AUTHENTICATION FIX
         if (not self.isLoggedIn(request) and request.get_full_path() != '/authentication/login/') :
-          return HttpResponseRedirect('/authentication/login/');  
+          return HttpResponseRedirect('/authentication/login/');
         #Manipulate params
-        params.update({ 
+        params.update({
             'logged_in'   : self.isLoggedIn(request),
             'user_login'  : self.getUserLogin(request),
+            'user_friend' : self.getUserFriend(request),
             'request_uri' : request.get_full_path()
         });
         #Render view
         return render(request, template, params);
-    
