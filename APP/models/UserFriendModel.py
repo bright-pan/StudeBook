df7b3de985f537643bc206fe304d5e596f427ab4
mjansma <m.jansma@live.nl>
2014-11-04 21:35:37 +0100

    getUserFriendByUser, toString methods added

--- APP/models/UserFriendModel.py
+++ APP/models/UserFriendModel.py
@@ -4,11 +4,19 @@ from django.db import models
 from APP.models.UserModel import User
 
 class UserFriend(models.Model):
+
     user_friend_id = models.AutoField(primary_key=True)
     requester = models.ForeignKey(User)
     recipient = models.ForeignKey(User)
-    status = models.IntegerField(blank=True)
+    status = models.CharField(blank=True)
     since = models.DateTimeField(auto_now=True)
+
+    @staticmethod
+    def getUserFriendByUser (user) :
+        return UserFriend.objects.filter(requester = user, status = 'accepted');
+
+    def __str__(self) :
+        return self.recipient.__str__();
 
     class Meta:
         managed = False
