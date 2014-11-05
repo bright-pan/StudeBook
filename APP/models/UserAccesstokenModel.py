fb870d52320516477ceb47dd9e559ac341ed92c3
mjansma <m.jansma@live.nl>
2014-11-05 13:47:35 +0100

    UserAccesstokenModel removed

--- APP/models/UserAccesstokenModel.py
+++ APP/models/UserAccesstokenModel.py
@@ -1,4 +1,4 @@
-#DJANGO
+ #DJANGO
 from django.db import models
 #SB
 from APP.models.UserModel import User
@@ -6,9 +6,8 @@ from APP.models.UserModel import User
 class UserAccessToken(models.Model):
 
     user = models.ForeignKey(User)
-    accesstoken = models.TextField(max_length=100)
+    access_token = models.TextField(max_length=100)
 
     class Meta:
         managed = False
-        db_table = 'sb_user_accesstoken'
-        
+        db_table = 'sb_user_access_token'
