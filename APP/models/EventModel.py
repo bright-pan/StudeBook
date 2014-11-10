866945a242264ec0dd1b6f19c56afdf0570d38a8
Justin X. Lambeck <lambeck.j@gmail.com>
2014-11-10 14:29:12 +0100

    Event pagina // urls.py is ook aangepast Authentication css aangepast

---
+++ APP/models/EventModel.py
@@ -0,0 +1,29 @@
+#DJANGO
+from django.db import models
+#SB
+from APP.models.UserModel import User
+
+class Event(models.Model):
+
+    event_id = models.AutoField(primary_key = True)
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey(User, blank=True, null=True)
+    body = models.TextField()
+    publiced = models.IntegerField()
+    datestart = models.DateField(blank=True, null=True)
+    dateend = models.DateField(blank=True, null=True)
+    img = models.CharField(max_length=500)
+
+    def __str__ (self) :
+        return self.title
+
+    def getPubliced (self) :
+        if (self.publiced) :
+            return 'Publiced'
+        return 'Not publiced'
+
+
+    class Meta:
+        managed = False
+        db_table = 'sb_event'
+
