dd33bc2769e35505ac53f76e31072979e51a65cb
mjansma <m.jansma@live.nl>
2014-11-12 15:49:38 +0100

    Mainpage => Profile

--- APP/models/UpdateModel.py
+++ APP/models/UpdateModel.py
@@ -12,7 +12,6 @@ class Update(models.Model):
     table_name = models.CharField(max_length=40)
     row_id = models.IntegerField(blank=True, null=True)
 
-
     class Meta:
         managed = False
         db_table = 'sb_update'
